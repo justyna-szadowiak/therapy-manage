275ab15576698cbe0af049fab0230439ec408a08
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-04-07 11:31:28 +0200

    Updated admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Patient, Therapy, Therapist, Plan } from '.././interfaces';
 import { TherapyManagerBackendService } from '../therapy-manager-backend.service';
@@ -7,10 +7,9 @@ import { TherapyManagerBackendService } from '../therapy-manager-backend.service
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class AdminComponent implements OnInit {
-  // selected$: Observable<Date[]>;
+  selected: Date | null | undefined;
   patients$: Observable<Patient[]>;
   therapies$: Observable<Therapy[]>;
   therapists$: Observable<Therapist[]>;
@@ -21,27 +20,7 @@ export class AdminComponent implements OnInit {
     this.therapies$ = this.endpoints.getAllTherapies();
     this.therapists$ = this.endpoints.getAllTherapists();
     this.planners$ = this.endpoints.getAllPlanners();
-
-    // this.selected$ = this.planners$.pipe(
-    //   map((planner: Plan[]) =>
-    //     planner.map((plan: Plan) => new Date(plan.date_time))
-    //   )
-    // );
   }
 
   ngOnInit(): void {}
-
-  // onDateSelected(event: any) {
-  //   console.log(event);
-  // }
-
-  // isSelected = (event: any) => {
-  //   const date =
-  //     event.getFullYear() +
-  //     '-' +
-  //     ('00' + (event.getMonth() + 1)).slice(-2) +
-  //     '-' +
-  //     ('00' + event.getDate()).slice(-2);
-  //   return this.daysSelected.find((x) => x == date) ? 'selected' : null;
-  // };
 }
